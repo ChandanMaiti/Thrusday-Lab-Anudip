2d709987204579043307d725957a02a39f55f037
Chandan Maiti <66934215+ChandanMaiti@users.noreply.github.com>
2022-07-29 23:56:11 +0530

    Add files via upload

---
+++ 07.07.22/AlphabetDigitOrSpecial.java
@@ -0,0 +1,27 @@
+/*wap to input any character and check whether its alphabet,digit or special character. */
+
+package BasicProgram;
+
+import java.util.Scanner;
+
+public class AlphabetDigitOrSpecial {
+	public static void main(String[] args) {
+	char ch;
+	Scanner scan=new Scanner(System.in);
+	ch = scan.next().charAt(0);
+	System.out.println("Enter Any Character");
+	if (Character.isAlphabetic(ch))
+	{
+		System.out.println(ch+" is Alphabet.");
+	}
+	else if(Character.isDigit(ch))
+	{
+		System.out.println(ch+" is Digit.");
+	}
+	else
+	{
+		System.out.println(ch+" is Special Character.");
+	}
+	
+}
+}
